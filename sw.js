e242af3b8d2b676b1f76e723cc3ea89547d0dea7
HaJunyeop <37467713+HaJunyeop@users.noreply.github.com>
2026-06-22 21:40:33 +0900

    최신 화면 우선 캐시 방식으로 변경

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-const CACHE = 'one-hand-tetris-v7';
+const CACHE = 'one-hand-tetris-v8';
 const ASSETS = ['./', './index.html', './style.css', './game.js', './manifest.webmanifest', './icon.svg'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+self.addEventListener('install', event => event.waitUntil(
+  caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+));
+self.addEventListener('activate', event => event.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+    .then(() => self.clients.claim())
+));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))
+  );
+});
